efff4af11d9ea616a3d2452ffe97d0726d21801e
jlouhela <louhela.joni@gmail.com>
2021-02-20 13:29:16 +0200

    Parse json

--- src/commands/quote.rs
+++ src/commands/quote.rs
@@ -1,9 +1,16 @@
+use serde::Deserialize;
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
+
 use std::env;
 
 use crate::ReqwestClientContainer;
+
+#[derive(Deserialize)]
+struct QuoteJson {
+    quote: String,
+}
 
 #[command]
 async fn quote(ctx: &Context, msg: &Message) -> CommandResult {
@@ -13,13 +20,15 @@ async fn quote(ctx: &Context, msg: &Message) -> CommandResult {
 
     if let Some(client) = data.get::<ReqwestClientContainer>() {
         match client.get(quote_url).bearer_auth(token).send().await {
-            Ok(resp) => match resp.text().await {
-                Ok(text) => {
-                    msg.channel_id.say(&ctx.http, format!("{}", text)).await?;
+            Ok(resp) => match resp.json::<Vec<QuoteJson>>().await {
+                Ok(json) => {
+                    msg.channel_id
+                        .say(&ctx.http, format!("{}", json[0].quote))
+                        .await?;
                 }
-                Err(_) => println!("ERROR reading {}", quote_url),
+                Err(_) => println!("ERROR reading quote from {}", quote_url),
             },
-            Err(_) => println!("ERROR downloading {}", quote_url),
+            Err(_) => println!("ERROR parsing quote result"),
         }
     } else {
         msg.reply(ctx, "There was a problem getting the reqwest client")
